6fe44152fc356d9afbec09610cb705668f0eb58c
amirMohammad <amyrmhmdahmdy179@gmail.com>
2025-04-17 08:11:34 -0700

    Displaying loading in menu.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,17 @@
-import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+
+import Header from "./Header";
+import Loader from "./loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading'
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
 
       <main>
