a874000ecf375c971d5307ea791bbb9e521b0768
amirMohammad <amyrmhmdahmdy179@gmail.com>
2025-04-21 10:12:59 -0700

    added tailwind responsives code and make a part of layout with flex.

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 function CartOverview() {
   return (
-    <div className="uppercase bg-stone-800 text-stone-200 p-4">
-      <p className="text-stone-300 space-x-4 font-semibold">
+    <div className="flex items-center justify-between twin uppercase bg-stone-800 text-stone-200 text-sm md:text-base px-4 py-4 sm:px-8">
+      <p className="text-stone-300 space-x-4 font-semibold sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
