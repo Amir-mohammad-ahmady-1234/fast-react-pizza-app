66ffaa7fe22dd98874453fb783111799f029a089
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-04-25 11:13:23 -0700

    Reading and Updating the User State.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,6 +3,7 @@ import { Form, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import { redirect } from 'react-router-dom';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -40,6 +41,8 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
 
+  const userName = useSelector((state) => state.user.userName);
+
   const formError = useActionData();
 
   return (
@@ -50,7 +53,7 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input defaultValue={userName} className="input grow" type="text" name="customer" required />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,29 +1,42 @@
 import { useState } from 'react';
 import Button from '../../ui/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateName } from './userSlice';
+import { useNavigate } from 'react-router-dom';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
+  const userName = useSelector((state) => state.user.userName);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username.trim()) return;
+
+    dispatch(updateName(username));
+    navigate('/menu');
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <p className='text-stone-600 mb-4 text-sm md:text-base'>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        className='input w-74 mb-8'
+        className="input w-74 mb-8"
       />
 
       {username !== '' && (
         <div>
           <Button type="primary">Start ordering</Button>
-        </div> 
+        </div>
       )}
     </form>
   );
